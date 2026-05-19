1d75634ebc8b2db6d81dfafe90635b7798f91e37
Bhushan Prasad Kashyap <your_email@example.comcd>
2026-05-19 21:06:16 +0300

    Rename Dedupe admin label to Data Analysis Module

--- dedupe/apps.py
+++ dedupe/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class DedupeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dedupe'
+   
     verbose_name = 'Data Analysis Module'
