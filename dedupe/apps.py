8ca97701825852917365ae17b9fa4f2330c42160
Bhushan <bhushan.kashyap@gmail.com>
2026-05-19 21:35:37 +0300

    Update apps.py

--- dedupe/apps.py
+++ dedupe/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class DedupeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dedupe'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dedupe"
+    verbose_name = "Data Analysis Module"
