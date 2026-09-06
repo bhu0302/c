0c6a12bd674fff6f3a83e94a198d67b4c6744347
Bhushan <bhushan.kashyap@gmail.com>
2026-09-06 18:17:07 +0300

    Refactor admin models for StgCustomerMaster, StgFinancial, and StgAddress
    enhanced code to upload and display information

--- ingestion/admin.py
+++ ingestion/admin.py
@@ -1,33 +1,119 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import StgCustomerMaster, StgFinancial, StgAddress
 
 
 @admin.register(StgCustomerMaster)
 class StgCustomerMasterAdmin(admin.ModelAdmin):
+
     list_display = (
-        "bp_id", "id_type", "id_number", "email", "mobile_number",
-        "contract_account", "installation", "move_in_date", "move_out_date",
-        "bp_creation_date", "bp_creation_source",
+        "bp_id",
+        "bp_number",
+        "bp_creation_date",
+        "bp_creation_source",
+        "id_type",
+        "id_number",
+        "email",
+        "mobile_number",
+        "date_of_birth",
+        "nationality",
+        "gender",
+        "contract_account",
+        "contract",
+        "installation",
+        "move_in_date",
+        "move_out_date",
     )
-    search_fields = ("bp_id", "id_number", "email", "mobile_number", "contract_account", "installation")
-    list_filter = ("id_type", "bp_creation_source", "nationality", "gender")
+
+    search_fields = (
+        "bp_id",
+        "bp_number",
+        "id_number",
+        "email",
+        "mobile_number",
+        "contract_account",
+        "contract",
+        "installation",
+    )
+
+    list_filter = (
+        "id_type",
+        "bp_creation_source",
+        "nationality",
+        "gender",
+        "bp_creation_date",
+        "move_in_date",
+        "move_out_date",
+    )
+
+    ordering = ("bp_id",)
+
     list_per_page = 50
 
 
 @admin.register(StgFinancial)
 class StgFinancialAdmin(admin.ModelAdmin):
-    list_display = ("bp_id", "contract_account", "payment_date", "payment_amount")
-    search_fields = ("bp_id", "contract_account")
-    list_filter = ("payment_date",)
+
+    list_display = (
+        "bp_id",
+        "contract_account",
+        "payment_date",
+        "payment_amount",
+    )
+
+    search_fields = (
+        "bp_id",
+        "contract_account",
+    )
+
+    list_filter = (
+        "payment_date",
+    )
+
+    ordering = (
+        "bp_id",
+        "-payment_date",
+    )
+
     list_per_page = 50
 
 
 @admin.register(StgAddress)
 class StgAddressAdmin(admin.ModelAdmin):
-    list_display = ("bp_id", "addr_line1", "street", "area", "city", "postal_code", "country")
-    search_fields = ("bp_id", "addr_line1", "street", "area", "city", "postal_code")
-    list_filter = ("city", "area", "country")
+
+    list_display = (
+        "bp_id",
+        "addr_line1",
+        "addr_line2",
+        "building",
+        "flat",
+        "street",
+        "area",
+        "city",
+        "postal_code",
+        "country",
+    )
+
+    search_fields = (
+        "bp_id",
+        "addr_line1",
+        "addr_line2",
+        "building",
+        "flat",
+        "street",
+        "area",
+        "city",
+        "postal_code",
+        "country",
+    )
+
+    list_filter = (
+        "city",
+        "area",
+        "country",
+    )
+
+    ordering = (
+        "bp_id",
+    )
+
     list_per_page = 50
