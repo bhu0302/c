7e5e361fa02e2aafdfac611d94597ca081873338
Bhushan <bhushan.kashyap@gmail.com>
2026-09-12 15:40:07 +0000

    Enhance development dashboard and test data

--- profiling/urls.py
+++ profiling/urls.py
@@ -5,4 +5,10 @@ app_name = "profiling"
 
 urlpatterns = [
     path("", views.profiling_dashboard, name="dashboard"),
+    path("email/", views.email_quality, name="email_quality"),
+    path("mobile/", views.mobile_quality, name="mobile_quality"),
+    path("address/", views.address_quality, name="address_quality"),
+    path("shared-email/", views.shared_email_detail, name="shared_email"),
+    path("shared-mobile/", views.shared_mobile_detail, name="shared_mobile"),
+
 ]
